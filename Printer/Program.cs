de5151e06910a0c1a4060bcc01c33983d54eec68
“venkatesan <vselvam@vmware.com>
2019-02-13 21:22:56 +0530

    project

---
+++ Printer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Printer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\nI am venkatesan.");
+            while (true)
+            {
+                Console.WriteLine("enter you name here");
+                var name = Console.ReadLine();
+                Console.WriteLine($"the name that you entered is {name}");
+            }
+        }
+    }
+}
